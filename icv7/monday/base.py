7d2597195500a5fa15c3ea1dd065055726bea6ff
booey811 <gabrielbarr811@gmail.com>
2021-09-09 17:37:42 +0100

    column mapping structure adjusted

--- icv7/monday/base.py
+++ icv7/monday/base.py
@@ -10,15 +10,32 @@ board = system.get_boards('name', 'items.name', ids=[test_board_id])[0]
 test_item_id = 1139943185
 
 
-class BaseItem:
-    def __init__(self, item_id):
-        self.moncli_obj = clients.monday.system.get_items(get_column_values=True, ids=[item_id])[0]
-        self.id = str(item_id)
-        self.name = self.moncli_obj.name
+class BaseItemStructure:
+    def __init__(self, item_id: str = None, board_id: str = None):
+        self.moncli_obj = None
+        self.name = ''
+        self.id = ''
+        self.columns = BaseColumnCollection
 
-        self.columns = BaseColumnCollection(self.moncli_obj.board['id'])
 
-    def stage_changes(self, changes_list: list[str, (int,str)]):
+class BaseItem(BaseItemStructure):
+
+    def __init__(self, item_id: str = None, board_id: str = None):
+        super().__init__(item_id, board_id)
+
+        if not item_id and not board_id:
+            raise Exception('Cannot Instantiate BaseItem without item_id or board_id')
+        elif item_id:
+            self.moncli_obj = clients.monday.system.get_items(get_column_values=True, ids=[item_id])[0]
+            self.id = str(item_id)
+            self.name = self.moncli_obj.name
+            self.columns = BaseColumnCollection(self.moncli_obj.board['id'])
+        elif board_id:
+            self.columns = BaseColumnCollection(board_id)
+        else:
+            raise Exception('Unexpected Inputs for BaseItem.__init__')
+
+    def stage_changes(self, changes_list: list[str, (int, str)]):
 
         # Validate input and convert to list of lists (if not provided in this format)
         if len(changes_list) == 2:
@@ -34,10 +51,8 @@ class BaseItem:
             desired_value = column_pair[1]
             self.columns.stage_change(column_id, desired_value)
 
-
     def commit_changes(self):
         pass
-
 
 
 class ModuleTest:
--- icv7/monday/mappings.py
+++ icv7/monday/mappings.py
@@ -1,3 +1,5 @@
+from moncli.entities import column_value
+
 from icv7.utilities import clients
 
 
@@ -9,6 +11,14 @@ class MappingObject:
 
         self.board_id = str(board_id)
         self._raw_mapping_dict = MAPPING_DICT[self.board_id]
+
+    def unpack_properties(self, eric_item_object):
+
+        for item in self._raw_mapping_dict['columns']:
+
+            name = self._raw_mapping_dict['columns'][item]
+
+            moncli_column = eric_item_object.moncli_obj.get_column_value()
 
 
 MAPPING_DICT = {
@@ -32,4 +42,94 @@ MAPPING_DICT = {
             'link': 'link'
         }
     }
+}
+
+
+class BaseColumnValue:
+    def __init__(self, moncli_column_value):
+        self.moncli_value = moncli_column_value
+        self.id = moncli_column_value.id
+        self.title = moncli_column_value.title
+
+    def change_value(self, new_value: (str, int)) -> list:
+        """
+Returns a list: [COLUMN_ID, COLUMN_VALUE]
+COLUMN_VALUE may be a string (text column), an integer (number column), a dictionary (status, dropwdown columns)
+        :param new_value: the value to set the column to
+        """
+        pass
+
+
+class TextColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+    def change_value(self, new_value):
+        changes = [self.id, new_value]
+        return changes
+
+
+class LongTextColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class StatusColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class DropdownColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class NumberColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class DateColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class LinkColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class FileColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class CheckboxColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class HourColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+class PeopleColumn(BaseColumnValue):
+    def __init__(self, moncli_column_value):
+        super().__init__(moncli_column_value)
+
+
+# Dictionary to convert moncli column values to Eric column values
+COLUMN_TYPE_MAPPINGS = {
+    column_value.TextValue: TextColumn,
+    column_value.LongTextValue: LongTextColumn,
+    column_value.StatusValue: StatusColumn,
+    column_value.DropdownValue: DropdownColumn,
+    column_value.NumberValue: NumberColumn,
+    column_value.DateValue: DateColumn,
+    column_value.LinkValue: LinkColumn,
+    column_value.FileValue: FileColumn,
+    column_value.CheckboxValue: CheckboxColumn,
+    column_value.HourValue: HourColumn,
+    column_value.PeopleValue: PeopleColumn
 }
