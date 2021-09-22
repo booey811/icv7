1ec53655ed13a145167444a4767c8b2b8c5fda8c
booey811 <gabrielbarr811@gmail.com>
2021-09-22 16:30:53 +0100

    item tests cleaned up

--- tests/monday/test_base.py
+++ tests/monday/test_base.py
@@ -13,20 +13,21 @@ def system_client_test_moncli_item(dev_test_board_system_item_id):
 class TestCreationFromID:
 
     @pytest.fixture(scope='class')
-    def test_item(self, dev_test_board_system_item_id):
+    def example_item(self, dev_test_board_system_item_id):
         return base.BaseItem(dev_test_board_system_item_id)
 
-    def test_id_is_correct(self, test_item, dev_test_board_system_item_id):
-        assert test_item._id == dev_test_board_system_item_id
+    def test_id_is_correct(self, example_item, dev_test_board_system_item_id):
+        assert example_item._id == dev_test_board_system_item_id
 
-    def test_board_id_is_correct(self, test_item, dev_test_board_id):
-        assert test_item._moncli_board_obj.id == dev_test_board_id
+    def test_board_id_is_correct(self, example_item, dev_test_board_id):
+        assert example_item._moncli_board_obj.id == dev_test_board_id
 
-    def test_moncli_item_object_is_present_and_correct(self, test_item):
-        assert type(test_item._moncli_obj) == moncli.entities.Item
+    def test_moncli_item_object_is_present_and_correct(self, example_item):
+        assert type(example_item._moncli_obj) == moncli.entities.Item
 
-    def test_moncli_board_object_is_present_and_correct(self, test_item):
-        assert type(test_item._moncli_board_obj) == moncli.entities.Board
+    def test_moncli_board_object_is_present_and_correct(self, example_item):
+        assert type(example_item._moncli_board_obj) == moncli.entities.Board
 
-    def test_no_column_changes_are_staged_after_instantiation(self, test_item):
-        assert test_item._staged_changes == {}
+    def test_no_column_changes_are_staged_after_instantiation(self, example_item):
+        assert example_item._staged_changes == {}
+
