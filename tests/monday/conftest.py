997d8ab3600f4b76695dd6ad9d2714016f2a003f
booey811 <gabrielbarr811@gmail.com>
2021-09-24 17:56:10 +0100

    rearranged and renamed test fixtures

---
+++ tests/monday/conftest.py
@@ -0,0 +1,68 @@
+import pytest
+
+from icv7.utilities import clients
+from icv7.monday.base import BaseItem
+
+
+@pytest.fixture(scope='session')
+def read_only_item_id():
+    return '1649471278'
+
+
+@pytest.fixture(scope='session')
+def system_item_id():
+    return '1139943185'
+
+
+@pytest.fixture(scope='session')
+def email_item_id():
+    return '1649459824'
+
+
+@pytest.fixture(scope='session')
+def error_item_id():
+    return '1649463539'
+
+
+@pytest.fixture(scope='session')
+def dev_test_board_id():
+    return '1139943160'
+
+
+@pytest.fixture()
+def moncli_read_only_item(read_only_item_id):
+    return clients.monday.system.get_items(ids=[read_only_item_id])[0]
+
+
+@pytest.fixture()
+def moncli_system_item(system_item_id):
+    return clients.monday.system.get_items(ids=[system_item_id])[0]
+
+
+@pytest.fixture()
+def moncli_email_item(email_item_id):
+    return clients.monday.system.get_items(ids=[email_item_id])[0]
+
+
+@pytest.fixture()
+def moncli_error_item(error_item_id):
+    return clients.monday.system.get_items(ids=[error_item_id])[0]
+
+
+@pytest.fixture()
+def eric_read_only_item(read_only_item_id):
+    return BaseItem(read_only_item_id)
+
+@pytest.fixture(scope='class')
+def eric_system_item(system_item_id):
+    return BaseItem(system_item_id)
+
+
+@pytest.fixture()
+def eric_email_item(email_item_id):
+    return BaseItem(email_item_id)
+
+
+@pytest.fixture()
+def eric_error_item(error_item_id):
+    return BaseItem(error_item_id)
--- tests/monday/test_base.py
+++ tests/monday/test_base.py
@@ -6,14 +6,9 @@ from icv7.utilities import clients
 from icv7.monday import config
 
 
-@pytest.fixture()
-def system_client_test_moncli_item(dev_test_board_system_item_id):
-    return clients.monday.system.get_items(ids=[dev_test_board_system_item_id])[0]
-
-
 @pytest.fixture(scope='class')
-def example_item_from_item_id(dev_test_board_system_item_id):
-    return base.BaseItem(dev_test_board_system_item_id)
+def example_item_from_item_id(system_item_id):
+    return base.BaseItem(system_item_id)
 
 
 @pytest.fixture(scope='class')
@@ -28,42 +23,43 @@ def column_mapping_data_for_test_board(dev_test_board_id):
 
 class TestItemAttributesFromItemID:
 
-    def test_id_is_correct(self, example_item_from_item_id, dev_test_board_system_item_id):
+    def test_id_is_correct(self, eric_system_item, system_item_id):
         '''
         test that the eric item acquired through an item is search has the same id as the monday item used to create it
         :param example_item_from_item_id:
         :param dev_test_board_system_item_id:
         :return:
         '''
-        assert example_item_from_item_id._id == dev_test_board_system_item_id
+        assert eric_system_item.id == system_item_id
 
-    def test_board_id_is_correct(self, example_item_from_item_id, dev_test_board_id):
-        '''
+    def test_board_id_is_correct(self, eric_system_item, dev_test_board_id):
+        """
         test that the board acquired through the monday item has the correct id
         :param example_item_from_item_id:
         :param dev_test_board_id:
         :return:
-        '''
-        assert example_item_from_item_id._moncli_board_obj.id == dev_test_board_id
+        """
+        assert eric_system_item._moncli_board_obj.id == dev_test_board_id
 
-    def test_moncli_item_object_is_present_and_correct(self, example_item_from_item_id):
-        assert type(example_item_from_item_id._moncli_obj) == moncli.entities.Item
+    def test_moncli_item_object_is_present_and_correct(self, eric_read_only_item):
+        assert type(eric_read_only_item._moncli_obj) == moncli.entities.Item
 
-    def test_moncli_board_object_is_present_and_correct(self, example_item_from_item_id):
-        assert type(example_item_from_item_id._moncli_board_obj) == moncli.entities.Board
+    def test_moncli_board_object_is_present_and_correct(self, eric_read_only_item):
+        assert type(eric_read_only_item._moncli_board_obj) == moncli.entities.Board
 
-    def test_no_column_changes_are_staged_after_instantiation(self, example_item_from_item_id):
-        assert example_item_from_item_id._staged_changes == {}
+    def test_no_column_changes_are_staged_after_instantiation(self, eric_read_only_item):
+        assert eric_read_only_item._staged_changes == {}
 
 
 class TestItemAttributesFromBoardID:
 
-    def test_board_id_is_correct(self, example_item_from_board_id, dev_test_board_id):
-        assert example_item_from_board_id._board_id == dev_test_board_id
+    def test_board_id_is_correct(self, eric_read_only_item, dev_test_board_id):
+        assert eric_read_only_item._board_id == dev_test_board_id
 
-    def test_moncli_board_object_is_present_and_correct(self, example_item_from_board_id):
-        assert type(example_item_from_board_id._moncli_board_obj) == moncli.entities.Board
+    def test_moncli_board_object_is_present_and_correct(self, eric_read_only_item):
+        assert type(eric_read_only_item._moncli_board_obj) == moncli.entities.Board
 
-    def test_no_column_changes_are_staged_after_instantiation(self, example_item_from_board_id):
-        assert example_item_from_board_id._staged_changes == {}
+    def test_no_column_changes_are_staged_after_instantiation(self, eric_read_only_item):
+
+        assert eric_read_only_item._staged_changes == {}
 
