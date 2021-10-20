73cbdc526859ba4fa8a86abc0f13428503546306
booey811 <gabrielbarr811@gmail.com>
2021-10-20 10:56:18 +0100

    added app factory and config

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from icv7 import create_app
+from icv7 import create_app, clients
 from icv7.monday import BaseItem, CustomLogger
-from icv7.utilities import clients
+
+# App Creation
+app = create_app()
 
 
-eric = BaseItem(1793683519)
+# Routes
+@app.route('/', methods=['GET'])
+def index():
+    print('Hello World')
+    return 'Hello Returns'
+
+
+if __name__ == '__main__':
+    app.run()
+else:
+    pass
--- icv7/__init__.py
+++ icv7/__init__.py
@@ -1,2 +1,19 @@
+import os
+
+import flask
+
+from icv7 import config
+from icv7.utilities import clients
+
+
 def create_app():
-    return 'You got it!'
+    if os.environ['ENV'] == 'prod':
+        configuration = config.ProdConfig
+    else:
+        configuration = config.DevConfig
+
+    app = flask.Flask('eric')
+
+    app.config.from_object(configuration)
+
+    return app
