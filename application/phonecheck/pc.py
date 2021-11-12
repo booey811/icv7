4cd7b0488691912c805a7a1e13d844e81941a99a
booey811 <gabrielbarr811@gmail.com>
2021-11-12 15:47:46 +0000

    pdf nightmare sorted

--- application/phonecheck/pc.py
+++ application/phonecheck/pc.py
@@ -4,37 +4,6 @@ import pydf
 import requests
 import json
 
-import settings
-
-
-# class PDFCreator:
-#
-#     def __init__(self):
-#         pass
-#
-#     @staticmethod
-#     def _get_pdfkit_config():
-#         """wkhtmltopdf lives and functions differently depending on Windows or Linux. We
-#          need to support both since we develop on windows but deploy on Heroku.
-#
-#         Returns:
-#             A pdfkit configuration
-#         """
-#         WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
-#                                            stdout=subprocess.PIPE).communicate()[0].strip()
-#         return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
-#
-#     def make_pdf(self, html, options=None):
-#         """Produces a pdf from raw html.
-#         Args:
-#             html (str): Valid html
-#             options (dict, optional): for specifying pdf parameters like landscape
-#                 mode and margins
-#         Returns:
-#             pdf of the supplied html
-#         """
-#         return pdfkit.from_string(html, False, configuration=self._get_pdfkit_config(), options=options)
-
 
 class PhonecheckManager:
     """Object that will manage interactions with the Phonecheck database"""
@@ -91,11 +60,12 @@ class PhonecheckManager:
     #         f.write(pdf)
     #     return f'tmp/pc_reports/report-{report_id}.pdf'
 
-
-    def new_convert_to_pdf(self, html_string, report_id):
+    @staticmethod
+    def new_convert_to_pdf(html_string, report_id):
         pdf = pydf.generate_pdf(html_string)
         with open(f'tmp/pc_reports/report-{report_id}.pdf', 'wb') as f:
             f.write(pdf)
+
 
 class CannotFindReportThroughIMEI(Exception):
     def __init__(self):
@@ -103,6 +73,3 @@ class CannotFindReportThroughIMEI(Exception):
 
 
 phonecheck = PhonecheckManager()
-
-
-
