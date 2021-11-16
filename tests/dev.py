92fb10cd0899e2bf4a57faf32918b13c79724410
booey811 <2y#obNdwZI02RndfcBKe>
2021-11-16 22:25:00 +0000

    add dev tests

--- tests/dev.py
+++ tests/dev.py
@@ -1,2 +1,10 @@
-def test_dev_config_works():
-    print("done")
+
+
+def test_to_print_stock_levels_for_repair_to_an_item():
+    """test creates a MainBoard Item for iPhone 7 Screen & Battery Repair, then checks the relvant stock levels and prints
+    to the item """
+
+
+def test_checking_stock_level_for_a_given_monday_repair_and_adjusting_staus():
+    """test to adjust the stock level status of Mainboard item dependant on it's repairs"""
+
