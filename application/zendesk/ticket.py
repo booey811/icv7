3b8f94d783f700b201f9be27b5ec921d18ccffa4
booey811 <gabrielbarr811@gmail.com>
2021-12-10 18:10:43 +0000

    updating custom fields on the fly (for device and repair columns)

--- application/zendesk/ticket.py
+++ application/zendesk/ticket.py
@@ -1,5 +1,9 @@
+import os
 from typing import Union
+import base64
+import json
 
+import requests
 import zenpy.lib.api_objects
 from zenpy.lib.exception import RecordNotFoundException
 
@@ -17,7 +21,8 @@ class EricTicket:
         elif type(ticket_id_or_obj) is zenpy.lib.api_objects.Ticket:
             self.zenpy_ticket = ticket_id_or_obj
         else:
-            raise Exception(f'EricTicket Instantiated with {type(ticket_id_or_obj)} - should be str, int or Zenpy Ticket')
+            raise Exception(
+                f'EricTicket Instantiated with {type(ticket_id_or_obj)} - should be str, int or Zenpy Ticket')
 
         self.id = self.zenpy_ticket.id
 
@@ -102,3 +107,40 @@ def process_organisation_data(logger, zenpy_ticket_obj: zenpy.lib.api_objects.Ti
     else:
         logger.log("No Organisation Data found")
         return {}
+
+
+def update_custom_field():
+
+    def encode_to_64(string):
+
+        string_bytes = string.encode("ascii")
+        b64_bytes = base64.b64encode(string_bytes)
+        b64_string = b64_bytes.decode("ascii")
+
+        return b64_string
+
+
+
+    url = "https://icorrect.zendesk.com/api/v2/ticket_fields/4413411999249"  # 4413411999249 ID for device field
+
+    method = "PUT"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + encode_to_64(f"admin@icorrect.co.uk/token:{os.environ['ZENDESKADMIN']}")
+    }
+
+    data = {"ticket_field": {
+                "custom_field_options": [
+                    {"name": "Apple Pie", "value": "apple"},
+                    {"name": "Pecan Pie", "value": "pecan"}
+                ]
+            }
+        }
+
+    r = requests.request(method, url, headers=headers, data=json.dumps(data))
+
+    print()
+
+
+update_custom_field()
