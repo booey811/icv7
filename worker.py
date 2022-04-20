3dd835c110214234b5c9a8473e3499af4e07a17e
booey811 <2y#obNdwZI02RndfcBKe>
2022-04-20 11:56:41 +0100

    update for importing data file before running worker process

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ import redis
 from rq import Worker, Queue, Connection
 
 import settings
+import data
 
 listen = ['high', 'default', 'stock', 'low']
 
@@ -13,6 +14,8 @@ if os.environ["ENV"] == 'devlocal':
     conn = redis.Redis('localhost', 6379)
 else:
     conn = redis.from_url(redis_url)
+
+
 
 if __name__ == '__main__':
     with Connection(conn):
